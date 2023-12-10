14139906102b2295ff77a06a75b878ea12d60c16
Vinícius Gonçalves <oldz.registros@gmail.com>
2023-12-10 18:00:23 -0300

    refactor: removed currPage var from app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
+import settings from 'settings';
 import { renderPages } from './pagination/pages.js';
 import renderButtons from './pagination/buttons.js';
-export let currPage = 1;
 window.addEventListener('DOMContentLoaded', () => {
-    renderPages(currPage).then(renderButtons);
+    renderPages(settings.currPage).then(renderButtons);
 });
